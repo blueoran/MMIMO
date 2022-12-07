b652fdc3fafdb988494fe0cbfade45f1e05cc31c
lhq <lhqtin@foxmail.com>
2022-12-07 21:22:00 +0800

    add README

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # MMIMO
+
+## 接口
+
+```cpp
+./decoder.cpp
+complex<double> **Decoder(
+                        int mod_order, 
+                        int num_sender, 
+                        int num_receiver, 
+                        int num_ofdm_sym, 
+                        complex<double> **H, 
+                        complex<double> **Y, 
+                        complex<double> *w = nullptr
+                        )
+```
+
+$$
+Y=Hx+w
+$$
+|param|type|description|
+|:---:|:---:|:---:|
+|mod_order|int|调制阶数|
+|num_sender|int|发送天线数|
+|num_receiver|int|接收天线数|
+|num_ofdm_sym|int|OFDM并发符号数|
+|H|[num_receiver*num_sender]|信道矩阵|
+|Y|[num_ofdm_sym*num_receiver]|接收信号|
+|w|[num_receiver]|噪音|
+
+## 测试
+
+### Simulation
+
+- environment:
+  - matlab
+  - g++
+
+```bash
+cd sim/RenewLab
+matlab
+rl_ofdm_mimo
+(显示: MIMO algorithm(ZF, Conj, C): ) 输入: C
+```
+
+### Traces
+
+- environment:
+  - numpy
+  - scipy
+  - scikit-commpy
+  - scikit-learn
+  - h5py
+  - matplotlib
+
+```bash
+cd trace/
+python test.py
+```
+
+## TODO
+
+- rl_ofdm_mmimo_sim.m
+- Agora
+- armadillo(等矩阵分析lib)
+- better simulator for python
--- trace/test.py
+++ trace/test.py
@@ -59,7 +59,7 @@ def simulate(decoder_class,mod_num,bws,noise,gen_len,binom_prob=0.5,apply_noise=
     msg_recv_mod=np.array([bws[:,:,i].T@(msg_send_mod[i,:,:]+noise_random[i,:,:]) for i in range(bws.shape[2])]).astype('complex128')
     msg_recv_decode=np.array([decoder(bws,msg_recv_mod[:,:,i],apply_noise*noise.mean(axis=0).mean(axis=0)) for i in range(msg_recv_mod.shape[-1])])
     msg_recv=Modulation_type.demodulate(msg_recv_decode.reshape(-1), 'hard').reshape(msg_send.shape)
-    print(f'msg_send: {msg_send.shape}\nmsg_send_mod: {msg_send_mod.shape}\nnoise_random: {noise_random.shape}\nbws: {bws.shape}\nmsg_recv_mod: {msg_recv_mod.shape}\nmsg_recv_decode: {msg_recv_decode.shape}\nmsg_recv: {msg_recv.shape}')
+    # print(f'msg_send: {msg_send.shape}\nmsg_send_mod: {msg_send_mod.shape}\nnoise_random: {noise_random.shape}\nbws: {bws.shape}\nmsg_recv_mod: {msg_recv_mod.shape}\nmsg_recv_decode: {msg_recv_decode.shape}\nmsg_recv: {msg_recv.shape}')
     return msg_send,msg_recv
 
 
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     # generate_samples('./traces/ArgosCSI-8x6-2015-11-28-14-39-47_uhf_static/ArgosCSI-8x6-2015-11-28-14-39-47_static.hdf5',20)
     h5log, userCSI, noise, num_users, samps_per_user, timestep=openLog('./sample_traces/sample_20.hdf5',1000)
     bws=calc_martix(userCSI,0,10,'zf')
-    msg_send,msg_recv=simulate(ZFDecoder, 64,bws,noise,1000,0.5,False)
+    msg_send,msg_recv=simulate(CDecoder, 64,bws,noise,1000,0.5,False)
     ber=cal_BER(msg_send,msg_recv)
 
 
